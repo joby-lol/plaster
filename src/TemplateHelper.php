63e2720efeac673b14741a9def9844279c6f36c3
Joby Elliott <code@byjoby.com>
2016-01-29 11:01:21 -0700

    Fixed endless loop in parent() Not matching all the way to the end in regex was causing a loop in which ever-increasing numbers of slashes were tried as the possible parents of the root url '/'

--- src/TemplateHelper.php
+++ src/TemplateHelper.php
@@ -59,7 +59,7 @@ class TemplateHelper implements Interfaces\TemplateHelper
         $page      = false;
         $parentUrl = $url;
         do {
-            $parentUrl = preg_replace('/^(.*\/)[^\/]+/', '$1', $parentUrl);
+            $parentUrl = preg_replace('/^(.*\/)[^\/]+$/', '$1', $parentUrl);
             //if parent url is the same, parent was called on the home page
             if ($parentUrl == $url) {
                 return false;
